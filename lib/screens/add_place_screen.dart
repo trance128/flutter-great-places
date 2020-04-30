1ccc5305d3965fe296e5078dae52355f83b806d5
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-04-30 05:28:05 +0100

    Added add_place_screen + image_input widget

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/image_input.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a new place'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'title',
+                      ),
+                      controller: _titleController,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+            label: Text('Add place'),
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
